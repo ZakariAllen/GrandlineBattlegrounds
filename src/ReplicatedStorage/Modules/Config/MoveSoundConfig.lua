a85b0987ef84ca4ce5b8e1e6b63541d8d7b98127
ZakariAllen <zacharyallensherrill@gmail.com>
2025-06-05 08:26:48 -0500

    Update MoveSoundConfig.lua

--- src/ReplicatedStorage/Modules/Config/MoveSoundConfig.lua
+++ src/ReplicatedStorage/Modules/Config/MoveSoundConfig.lua
@@ -2,7 +2,7 @@ local MoveSoundConfig = {
     PartyTableKick = {
         Hit = "rbxassetid://1234567890",
         Miss = "rbxassetid://0987654321",
-        Loop = "rbxassetid://1122334455",
+        Loop = "rbxassetid://118537831520752",
     }
 }
 
