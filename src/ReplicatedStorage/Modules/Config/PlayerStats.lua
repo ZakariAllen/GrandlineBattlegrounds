1c13163e818ee96cf47f3045b58e60249cc0df94
ZakariAllen <zacharyallensherrill@gmail.com>
2025-06-06 11:03:44 -0500

    Update PlayerStats.lua

--- src/ReplicatedStorage/Modules/Config/PlayerStats.lua
+++ src/ReplicatedStorage/Modules/Config/PlayerStats.lua
@@ -4,6 +4,6 @@ PlayerStats.HP = 200
 PlayerStats.BlockHP = 50
 PlayerStats.Stamina = 150
 PlayerStats.HPRegen = 1 -- per second
-PlayerStats.StaminaRegen = 1.5 -- per second
+PlayerStats.StaminaRegen = 2.5 -- per second
 
 return PlayerStats
