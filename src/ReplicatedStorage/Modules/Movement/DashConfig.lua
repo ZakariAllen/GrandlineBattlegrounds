e1265bd803a3f9d93a82ace8482f271569627501
ZakariAllen <zacharyallensherrill@gmail.com>
2025-06-06 11:57:29 -0500

    Update DashConfig.lua

--- src/ReplicatedStorage/Modules/Movement/DashConfig.lua
+++ src/ReplicatedStorage/Modules/Movement/DashConfig.lua
@@ -7,35 +7,35 @@ DashConfig.SoundId = "rbxassetid://72014632956520"  -- Replace with your dash SF
 DashConfig.Settings = {
 	Forward = {
 		Distance = 15,
-		Duration = 0.15,
+		Duration = 0.18,
 	},
 	Backward = {
 		Distance = 15,
-		Duration = 0.15,
+		Duration = 0.18,
 	},
 	Left = {
 		Distance = 15,
-		Duration = 0.15,
+		Duration = 0.18,
 	},
 	Right = {
 		Distance = 15,
-		Duration = 0.15,
+		Duration = 0.18,
 	},
 	ForwardLeft = {
 		Distance = 15,
-		Duration = 0.15,
+		Duration = 0.18,
 	},
 	ForwardRight = {
 		Distance = 15,
-		Duration = 0.15,
+		Duration = 0.18,
 	},
 	BackwardLeft = {
 		Distance = 15,
-		Duration = 0.15,
+		Duration = 0.18,
 	},
 	BackwardRight = {
 		Distance = 15,
-		Duration = 0.15,
+		Duration = 0.18,
 	},
 }
 
