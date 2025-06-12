213f09a4d3a2ad422439be82e46915bc429eac69
Zakeii <45607494+ZakariAllen@users.noreply.github.com>
2025-06-12 12:24:49 -0500

    Disable automatic character spawn and update map path

---
+++ src/ServerScriptService/Misc/DisableAutoSpawn.server.lua
@@ -0,0 +1,7 @@
+local Players = game:GetService("Players")
+
+-- Prevent automatic character spawning when players join
+Players.CharacterAutoLoads = false
+
+print("[DisableAutoSpawn] Character auto-loading disabled")
+
